9d4c0e711b87d9d775752527052a369f97a125a3
Vedang685 <vedang685@gmail.com>
2023-06-05 10:04:54 +0530

    Leetcode Problem 169

---
+++ CycleSort.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+public class CycleSort
+{
+    public static void main(String[] args)
+    {
+        int[] nums = {3,5,1,2,4};
+        // numbers are from 1 to n hence correct place for number at nums[i] = nums[i]-1
+        sort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+    static void sort(int[] nums)
+    {
+        int i = 0;
+        while(i < nums.length)
+        {
+            int correct = nums[i] - 1; // correct index for element at nums[i]
+            if(nums[i] != nums[correct])
+            {
+                // if the element at i is not at its correct position
+                // swap with the element present at index i's correct position
+                // now it will check if the swapped number is at correct position
+                swap(nums,i,correct);
+            }
+            else
+            {
+                // traverse across the array to check if all elements are at correct position
+                i++;
+            }
+        }
+    }
+
+    static void swap(int[] nums, int i, int correct)
+    {
+        int temp = nums[i];
+        nums[i] = nums[correct];
+        nums[correct] = temp;
+    }
+
+}
